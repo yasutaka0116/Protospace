683ac0a03b0e39c5cf5d91f81973fc471f7fda99
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-16 15:47:37 +0900

    rollbackをしてavatarカラムを取り除いた。戻したい状態に戻った。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,5 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :device_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :works, :member, :profile, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :works, :member, :profile,])
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file  :avatar,
-                        styles: { medium: "300x300#", thumb: "100x100#" }
-  validates_attachment_content_type :avatar, content_type: ["imaage/jpg","image/jpeg","image/png"]
+
 end
