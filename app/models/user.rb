84d50fafe4803a9631a4694cea59eed9b922b4f9
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-16 18:49:20 +0900

    サインインとサインアウトの機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_validation :configure_permitted_parameters, if: :devise_controller?
 
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:name, :member, :profile, :works)
+    end
 end
