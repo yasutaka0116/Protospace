e529ce349b6978b27a51c9e8c8c30eca3d1cf6b0
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-17 18:41:08 +0900

    inithalize commit ルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
+  get  '/users/:id'   =>    'users#show'
 end
