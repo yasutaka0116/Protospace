a96add3ad3ae81ec19df9a2276baae12f4a0722e
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-17 13:43:35 +0900

    サインアップのためのviewファイルの変更、追加したカラのキーの承認のための記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  before_validation :configure_permitted_parameters, if: :devise_controller?
-
-
-  def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:name, :member, :profile, :works)
-    end
 end
