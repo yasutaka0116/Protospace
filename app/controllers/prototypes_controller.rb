9cc08222bddc910ae5f66b4432a4bc7f2e549bd9
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-22 17:19:32 +0900

    newアクションを設定、cretateアクションの設定、投稿画面のビューを設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,5 +4,19 @@ class PrototypesController < ApplicationController
   def show
   end
   def new
+    @prototype=Prototype.new
+    @prototype.capturedimages.build
   end
+
+  def create
+    Prototype.create(create_params)
+    redirect_to :root
+
+  end
+
+  private
+  def create_params
+    params.require(:prototype).permit(:name, :catchcopy, :content, capturedimages_attributes:[:image]).merge(capturedimages_attributes:[:role])
+  end
+
 end
