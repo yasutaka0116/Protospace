a1c6b1238c3d52fb838ba36ac8c1a363560acdbb
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-24 20:27:24 +0900

    editアクションとupdateアクションの定義、半角スペースあけて見やすいようにした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,11 +3,12 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+
   def show
-    @prototype=Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:id])
   end
   def new
-    @prototype=Prototype.new
+    @prototype = Prototype.new
     @prototype.capturedimages.build
   end
 
@@ -21,12 +22,26 @@ class PrototypesController < ApplicationController
     redirect_to :root
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype =Prototype.find(params[:id])
+    @prototype.update(update_params)
+    flash[:success]="edit complited"
+    redirect_to :root
+  end
+
 
 
 
   private
   def create_params
-    params.require(:prototype).permit(:name, :catchcopy, :content, :user_id, capturedimages_attributes:[:image, :role,])
+    params.require(:prototype).permit(:name, :catchcopy, :content, :user_id, capturedimages_attributes:[:image, :role])
+  end
+  def update_params
+    params.require(:prototype).permit(:name, :catchcopy, :content, :user_id, capturedimages_attributes:[:id, :image, :role])
   end
 
 
