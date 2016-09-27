1a2dad24709d2a01c2c091f0b9b8a857918afc48
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-27 14:14:13 +0900

    purototypeにlikes_countカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919104854) do
+ActiveRecord::Schema.define(version: 20160926101405) do
 
   create_table "capturedimages", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -21,13 +21,21 @@ ActiveRecord::Schema.define(version: 20160919104854) do
     t.datetime "updated_at",                 null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "prototypes", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.integer  "user_id",    limit: 4
-    t.text     "catchcopy",  limit: 65535
-    t.text     "content",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name",        limit: 255
+    t.integer  "user_id",     limit: 4
+    t.text     "catchcopy",   limit: 65535
+    t.text     "content",     limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "likes_count", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
