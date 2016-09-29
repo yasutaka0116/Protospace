88313957d82f73a4fca3dace868e6f16501cebef
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-29 16:01:02 +0900

    newestのルーティングを設定しviewページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
+  get 'prototypes/newest'  => 'prototypes/newest#index'
   resources :prototypes do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
