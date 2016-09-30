4787859140d53c1d9c6707108a24cfa8e9572263
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-09-30 18:14:40 +0900

    ユーザー画像のアップローダー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar,AvatarUploader
+
   has_many :prototypes
   has_many :likes
   has_many :comments
