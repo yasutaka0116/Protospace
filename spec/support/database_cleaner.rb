f985d4470354949c0de5e6bc8ecb77285282cb5b
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-10-23 15:16:39 +0900

    database_cleanerの設定の記述を統一

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
 
 config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with = :truncation
   end
 
   config.around(:each) do |example|
