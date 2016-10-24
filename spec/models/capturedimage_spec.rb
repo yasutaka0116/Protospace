c8d28c79d9ae40f328f343c8744ec20e43562b0b
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-10-24 12:23:03 +0900

    コメントアウト部分の削除、改行

--- spec/models/capturedimage_spec.rb
+++ spec/models/capturedimage_spec.rb
@@ -1,24 +1,5 @@
 require "rails_helper"
-require 'pry-rails'
-  # describe Myuploader do
-  #       include CarrierWave::Test::Matchers
-  #       let(:prototype){double('prototype')}
-  #       let(:uploader){Myuploader.new(prototype, :image)}
 
-  #     before do
-  #         MyUploader.enable_processing = true
-  #         File.open(path_to_file) { |f| uploader.store!(f) }
-  #     end
-
-  #     after do
-  #        MyUploader.enable_processing = false
-  #        uploader.remove!
-  #     end
-
-#   it "has the correct format" do
-#     expect(uploader).to be_format('png')
-#   end
-# end
   describe Capturedimage do
     describe "#create" do
 
@@ -28,14 +9,13 @@ require 'pry-rails'
         expect(image.errors[:image]).to include("can't be blank")
       end
 
-
-
       context "with valid attributes" do
         it "has valid factory" do
           image = build(:capturedimage, :with_mainimage)
           expect(image).to be_valid
         end
       end
+
       context "without image attribute" do
         it "returns error" do
           image = build(:capturedimage, :with_mainimage, image: "")
@@ -43,6 +23,7 @@ require 'pry-rails'
           expect(image.errors[:image]).to include("can't be blank")
         end
       end
+
       context "without role attributes" do
         it "returns error" do
           image = build(:capturedimage, :with_mainimage, role: "")
@@ -50,5 +31,6 @@ require 'pry-rails'
           expect(image.errors[:role]).to include("can't be blank")
        end
       end
-      end
+
     end
+  end
