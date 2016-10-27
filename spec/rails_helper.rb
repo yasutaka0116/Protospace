5c87cd56de7ca71c1b6ddc5b96058558c6ff59fc
yasu-PC\yasu <k.yasutaka0116@gmail.com>
2016-10-27 16:58:45 +0900

    rspecでのdeviseの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -35,7 +36,12 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
   config.include ActionDispatch::TestProcess
+
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
+
 
   FactoryGirl::SyntaxRunner.class_eval do
     include ActionDispatch::TestProcess
